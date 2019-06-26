9548ddc066fb09ddc98573d18b1038f1ab248a2e
virat02 <virat.goradia96@gmail.com>
2019-06-25 21:40:25 -0400

    Pointer to heroku

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'whiteboard';
-    let connectionString = 'mongodb://localhost/';
-    mongoose.connect(connectionString+databaseName);
+    // const databaseName = 'whiteboard';
+    // let connectionString = 'mongodb://localhost/';
+    let connectionString = 'mongodb://heroku_v9k8xd6d:h9aac1d0b1l0rsnfhq44vlj8nr@ds243607.mlab.com:43607/heroku_v9k8xd6d';
+    mongoose.connect(connectionString, {useNewUrlParser: true});
 };
--- server.js
+++ server.js
@@ -27,6 +27,6 @@ userController(app);
 websiteController(app);
 pageController(app);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 
